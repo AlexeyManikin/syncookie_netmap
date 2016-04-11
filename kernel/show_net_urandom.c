8e9cfa5d35a63dfdd7705bc303c33ebb59ea06d4
Alexander Polyakov <apolyakov@beget.ru>
2016-04-11 16:54:02 +0300

    Show syncookie_secret

--- kernel/show_net_urandom.c
+++ kernel/show_net_urandom.c
@@ -31,11 +31,22 @@
 #include <linux/module.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <linux/kallsyms.h>
+#include <linux/cryptohash.h>
 #include <net/tcp.h>
+
+static u32 (*syncookie_secret_ptr)[2][16-4+SHA_DIGEST_WORDS] __read_mostly;
 
 static int uptime_proc_show_beget(struct seq_file *m, void *v)
 {
+	int i, j;
 	seq_printf(m, "%lu %lu\n", (unsigned long) jiffies, (unsigned long)tcp_cookie_time());
+	for(i = 0; i < 2; i++) {
+		for(j = 0; j < 16-4+SHA_DIGEST_WORDS; j++) {
+			seq_printf(m, "%.8x.", (*syncookie_secret_ptr)[i][j]); 
+		}
+	}
+	seq_printf(m, "\n");
 	return 0;
 }
 
@@ -65,8 +76,23 @@ static int __init proc_uptime_init_beget(void)
  * only be run once, when the module is loaded.
  */
 
+
+static int symbol_walk_callback(void *data, const char *name,
+				struct module *mod, unsigned long addr) {
+	if (mod)
+		return 0;
+
+	if (strcmp(name, "syncookie_secret") == 0) {
+		syncookie_secret_ptr = (void *)addr;
+	}
+	return 0;
+}
+
 static int __init hello_init(void)
 {
+	int rc = kallsyms_on_each_symbol(symbol_walk_callback, NULL);
+	if (rc)
+		return rc;
 	return proc_create("beget_uptime", 0, NULL, &uptime_proc_fops_beget) == NULL;
 }
 
